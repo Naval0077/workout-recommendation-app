bd0a157c71f306f83247c8ff87cdefa4bf4e76cc
Naval Agarwal <naval@MacBookPro.local>
2025-04-03 21:38:25 +0100

    added port

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
-
+import os
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(threaded=True)  # Enables multiple request handling
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT; default to 10000 if missing
+    app.run(host="0.0.0.0", port=port, threaded=True)
