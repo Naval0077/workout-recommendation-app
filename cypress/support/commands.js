97c68ecca7cad6f49c111aa795e29240ae8d96ea
Naval Agarwal <naval@MacBookPro.local>
2025-04-08 11:46:58 +0100

    checking the timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,6 +30,7 @@ Cypress.Commands.add('login', (email, password) => {
   cy.get('input[name="email"]').type(email);
   cy.get('input[name="password"]').type(password);
   cy.get('input[type=submit]').click();
+  cy.wait(15000)
   cy.url().should('include', '/input', { timeout: 15000 });
 });
 
@@ -48,7 +49,6 @@ Cypress.Commands.add('input', () => {
     console.log("INPUT PAGE HTML:");
     console.log(doc.documentElement.innerHTML); // Output page source to Cypress logs
   });
-  cy.url().should('include', '/input', { timeout: 10000 });
   cy.get('input[name="height"]').type('180');  // Height field
   cy.get('input[name="weight"]').type('75');   // Weight field
   cy.get('input[name="age"]').type('25');      // Age field
