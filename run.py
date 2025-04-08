9a99609de7bcf82b619c2841c2d8474e23cfe454
Naval Agarwal <naval@MacBookPro.local>
2025-04-08 13:39:24 +0100

    checking..

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
 from app import create_app
 import os
 app = create_app()
-print("== Flask is starting with config:", os.getenv("FLASK_CONFIG"))
-if os.environ.get('FLASK_ENV') == 'testing':
-    app.config.from_object('app.config.TestingConfig')
 
 if __name__ == "__main__":
-    app.run( threaded=True)
+    app.run()
 
 
