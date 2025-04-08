8bfc54ae483fa2437b9b49e00dc88c97d5ba9ec1
Naval Agarwal <naval@MacBookPro.local>
2025-04-08 02:33:47 +0100

    change made in ci

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 app = create_app()
 print("== Flask is starting with config:", os.getenv("FLASK_CONFIG"))
 if os.environ.get('FLASK_ENV') == 'testing':
-    app.config.from_object('config.TestingConfig')
+    app.config.from_object('app.config.TestingConfig')
 
 if __name__ == "__main__":
     app.run( threaded=True)
