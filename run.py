9e59ef341bd1b31f6b0e1142731667351053a9f0
Naval Agarwal <naval@MacBookPro.local>
2025-04-09 19:12:40 +0100

    azure deployment 2

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ if env == "testing":
 else:
     app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
 
 
